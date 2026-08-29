b8f840f99a5ab40ffaa82882df0d9ea3ad3645d7
moufasarim-lang <moufasarim@gmail.com>
2026-08-29 12:39:03 -0400

    Remove blinkdotnew ui to fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
+
 import App from './App'
 import './index.css'
+import './stl.css'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BlinkUIProvider theme="linear" darkMode="light">
-        <Toaster />
+      
+        
         <App />
-      </BlinkUIProvider>
+      
     </QueryClientProvider>
   </React.StrictMode>,
 )
+
